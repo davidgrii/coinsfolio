6c5318b0d41150936883678db6f9b0193e9513f8
davidgrii <grikurovdavid@gmail.com>
2025-12-10 17:07:44 +0100

    add crypto fix 3

--- src/components/portfolio/edit-crypto.tsx
+++ src/components/portfolio/edit-crypto.tsx
@@ -99,104 +99,105 @@ export const EditCrypto: React.FC<IProps> = ({
             Edit Portfolio Modal
           </DialogTitle>
         </VisuallyHidden>
+        <div className='flex flex-col items-center overflow-y-auto max-h-[300px] scrollbar-none'>
+          <Placeholder header={t('edit_crypto.edit_coin')} />
 
-        <Placeholder header={t('edit_crypto.edit_coin')} />
+          <form className={cn('w-full flex flex-col gap-6 items-center justify-between px-3', platform === 'ios' ? 'pb-5' : 'pb-3')}>
+            {selectedCrypto && (
+              <div className={cn('relative flex items-center justify-between w-full px-3 py-4 h-[50px] !bg-neutral-04 rounded-xl',
+                !(platform === 'ios' || platform === 'macos') && 'shadow-[0_0_0_2px_rgba(255,255,255,0.1)]'
+              )}>
+                <div className='flex items-center gap-3'>
+                  <div className='rounded-full overflow-hidden'>
+                    <Avatar
+                      size={28}
+                      src={selectedCrypto.crypto.image}
+                      alt={selectedCrypto.crypto.name}
+                      className='!bg-transparent'
+                    />
+                  </div>
 
-        <form className='w-full flex flex-col gap-6 items-center justify-between px-3 pb-3'>
-          {selectedCrypto && (
-            <div className={cn('relative flex items-center justify-between w-full px-3 py-4 h-[50px] !bg-neutral-04 rounded-xl',
-              !(platform === 'ios' || platform === 'macos') && 'shadow-[0_0_0_2px_rgba(255,255,255,0.1)]'
-            )}>
-              <div className='flex items-center gap-3'>
-                <div className='rounded-full overflow-hidden'>
-                  <Avatar
-                    size={28}
-                    src={selectedCrypto.crypto.image}
-                    alt={selectedCrypto.crypto.name}
-                    className='!bg-transparent'
-                  />
-                </div>
-
-                <div className={'flex-col'}>
-                  <p className='text-xs text-foreground '>
-                    {selectedCrypto.crypto.symbol.toUpperCase()}
-                  </p>
-                  <p className='text-[8px] text-neutral-03'>
-                    {selectedCrypto.crypto.name}
-                  </p>
+                  <div className={'flex-col'}>
+                    <p className='text-xs text-foreground '>
+                      {selectedCrypto.crypto.symbol.toUpperCase()}
+                    </p>
+                    <p className='text-[8px] text-neutral-03'>
+                      {selectedCrypto.crypto.name}
+                    </p>
+                  </div>
                 </div>
               </div>
+            )}
+
+            <div className='w-full relative'>
+              <Input
+                type='text'
+                placeholder={t('add_crypto.quantity')}
+                value={quantity}
+                onChange={handleChangeQuantity}
+                after={quantity ? (
+                  <Tappable
+                    Component='div'
+                    className='bg-neutral-04  rounded-xl'
+                    onClick={() => setQuantity('')}
+                  >
+                    <Icons.close />
+                  </Tappable>
+                ) : (
+                  <Icons.question
+                    onClick={() => alert('Enter your total crypto quantity Example: 123,450.32 or 0.00213')}
+                    className="size-5 mr-0.5 text-neutral-03 cursor-pointer duration-200 transition-opacity ease-out hover:opacity-80"
+                  />
+                )}
+                className='!bg-neutral-04 !w-full'
+              />
             </div>
-          )}
 
-          <div className='w-full relative'>
-            <Input
-              type='text'
-              placeholder={t('add_crypto.quantity')}
-              value={quantity}
-              onChange={handleChangeQuantity}
-              after={quantity ? (
-                <Tappable
-                  Component='div'
-                  className='bg-neutral-04  rounded-xl'
-                  onClick={() => setQuantity('')}
-                >
-                  <Icons.close />
-                </Tappable>
-              ) : (
-                <Icons.question
-                  onClick={() => alert('Enter your total crypto quantity Example: 123,450.32 or 0.00213')}
-                  className="size-5 mr-0.5 text-neutral-03 cursor-pointer duration-200 transition-opacity ease-out hover:opacity-80"
-                />
-              )}
-              className='!bg-neutral-04 !w-full'
-            />
-          </div>
+            <div className='w-full relative'>
+              <Input
+                type='text'
+                placeholder={t('add_crypto.purchase')}
+                value={purchase}
+                onChange={handleChangePurchase}
+                after={purchase ? (
+                  <Tappable
+                    Component='button'
+                    className='bg-neutral-04 rounded-full'
+                    onClick={() => setPurchase('')}
+                  >
+                    <Icons.close />
+                  </Tappable>
+                ) : (
+                  <Icons.question
+                    onClick={() => alert('Enter your crypto purchase price Example: 123,450.32 or 0.00213')}
+                    className="size-5 mr-0.5 text-neutral-03 cursor-pointer duration-200 transition-opacity ease-out hover:opacity-80"
+                  />
+                )}
+                className='!bg-neutral-04 !w-full'
+              />
+            </div>
 
-          <div className='w-full relative'>
-            <Input
-              type='text'
-              placeholder={t('add_crypto.purchase')}
-              value={purchase}
-              onChange={handleChangePurchase}
-              after={purchase ? (
-                <Tappable
-                  Component='button'
-                  className='bg-neutral-04 rounded-full'
-                  onClick={() => setPurchase('')}
-                >
-                  <Icons.close />
-                </Tappable>
-              ) : (
-                <Icons.question
-                  onClick={() => alert('Enter your crypto purchase price Example: 123,450.32 or 0.00213')}
-                  className="size-5 mr-0.5 text-neutral-03 cursor-pointer duration-200 transition-opacity ease-out hover:opacity-80"
-                />
-              )}
-              className='!bg-neutral-04 !w-full'
-            />
-          </div>
+            <div className='w-full relative'>
+              <Textarea
+                placeholder={t('add_crypto.note')}
+                value={notice}
+                onChange={(e) => setNotice(e.target.value)}
+                className='!w-full !bg-neutral-04'
+              />
+            </div>
 
-          <div className='w-full relative'>
-            <Textarea
-              placeholder={t('add_crypto.note')}
-              value={notice}
-              onChange={(e) => setNotice(e.target.value)}
-              className='!w-full !bg-neutral-04'
-            />
-          </div>
-
-          <Button
-            size='l'
-            mode='filled'
-            onClick={handleSubmit}
-            className={
-              'bg-foreground py-8 rounded-xl text-lg text-background font-semibold mx-auto w-full transition-colors hover:bg-foreground/75'
-            }
-          >
-            {t('edit_crypto.btn')} {selectedCrypto?.crypto.symbol?.toUpperCase()}
-          </Button>
-        </form>
+            <Button
+              size='l'
+              mode='filled'
+              onClick={handleSubmit}
+              className={
+                'bg-foreground py-8 rounded-xl text-lg text-background font-semibold mx-auto w-full transition-colors hover:bg-foreground/75'
+              }
+            >
+              {t('edit_crypto.btn')} {selectedCrypto?.crypto.symbol?.toUpperCase()}
+            </Button>
+          </form>
+        </div>
       </Modal>
     </>
   );
