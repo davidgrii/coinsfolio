a0538d745d642a892fd0821ec4eba96913644b5f
davidgrii <grikurovdavid@gmail.com>
2025-12-14 17:39:46 +0100

    added smart alerts logic

--- src/hooks/queries/use-smart-alerts.ts
+++ src/hooks/queries/use-smart-alerts.ts
@@ -1,10 +1,37 @@
-import { useMutation } from '@tanstack/react-query';
-import { useUser } from '@/app/_providers/user-provider';
-import { BASE_URL } from '@/constants';
-import type { IPercentageAlert, IPriceAlert } from '@/types';
+import { useMutation, useQuery, useQueryClient, type UseQueryResult } from '@tanstack/react-query'
+import { useUser } from '@/app/_providers/user-provider'
+import { BASE_URL } from '@/constants'
+import type { IPercentageAlert, IPriceAlert, IVolatilityAlert } from '@/types'
+
+export interface ISmartAlertResponse {
+  price_alerts: IPriceAlert[];
+  percentage_alerts: IPercentageAlert[];
+  volatility_alerts: IVolatilityAlert[];
+}
+
+export const useSmartAlerts = (): UseQueryResult<ISmartAlertResponse, Error> => {
+  const { userId } = useUser()
+
+  return useQuery({
+    queryKey: ['smartAlerts', userId],
+    queryFn: async () => {
+      const res = await fetch(`${BASE_URL}/api/users/${userId}/smart-alerts`, {
+        method: 'GET'
+      })
+
+      if (!res.ok) {
+        throw new Error('Error creating price alert')
+      }
+
+      return await res.json()
+    },
+    select: (result) => result.data
+  })
+}
 
 export const useCreatePriceAlert = () => {
-  const { userId } = useUser();
+  const { userId } = useUser()
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: async ({ data }: { data: IPriceAlert }) => {
@@ -13,21 +40,25 @@ export const useCreatePriceAlert = () => {
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(data),
-        },
-      );
+          body: JSON.stringify(data)
+        }
+      )
 
       if (!res.ok) {
-        throw new Error('Error creating price alert');
+        throw new Error('Error creating price alert')
       }
 
-      return await res.json();
+      return await res.json()
     },
-  });
-};
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['smartAlerts', userId] })
+    }
+  })
+}
 
 export const useCreatePercentageAlert = () => {
-  const { userId } = useUser();
+  const { userId } = useUser()
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: async ({ data }: { data: IPercentageAlert }) => {
@@ -36,21 +67,25 @@ export const useCreatePercentageAlert = () => {
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(data),
-        },
-      );
+          body: JSON.stringify(data)
+        }
+      )
 
       if (!res.ok) {
-        throw new Error('Error creating percentage alert');
+        throw new Error('Error creating percentage alert')
       }
 
-      return await res.json();
+      return await res.json()
     },
-  });
-};
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['smartAlerts', userId] })
+    }
+  })
+}
 
 export const useCreateVolatilityAlert = () => {
-  const { userId } = useUser();
+  const { userId } = useUser()
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: async ({ data }: { data: IPercentageAlert }) => {
@@ -59,15 +94,43 @@ export const useCreateVolatilityAlert = () => {
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(data),
-        },
-      );
+          body: JSON.stringify(data)
+        }
+      )
 
       if (!res.ok) {
-        throw new Error('Error creating percentage alert');
+        throw new Error('Error creating percentage alert')
       }
 
-      return await res.json();
+      return await res.json()
     },
-  });
-};
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['smartAlerts', userId] })
+    }
+  })
+}
+
+export const useDeleteSmartAlert = () => {
+  const { userId } = useUser()
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({ data }: { data: { alertId: string, alertType: string }}) => {
+      const res = await fetch(
+        `${BASE_URL}/api/users/${userId}/smart-alerts`,
+        {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        }
+      )
+
+      if (!res.ok) {
+        throw new Error('Error deleting price alert')
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['smartAlerts', userId] })
+    }
+  })
+}
